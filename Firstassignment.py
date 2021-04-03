36f439720d8cc10da57ab0e9fc22f4af54732216
Mandalaalex1 <mandalaalex@yahoo.com>
2021-04-02 19:55:26 -0500

    Firstassignment.py

---
+++ Firstassignment.py
@@ -0,0 +1,8 @@
+#Enter first and last name
+names = input("Alexander Mandala")
+print(names)
+print()
+#Enter your goals for this class
+print("What are your goals for this class - long term goals?")
+goals = input('To land in a high paying job and to develop my career I want to be able to create programs that will assist save the world esp back home')
+print(goals)
